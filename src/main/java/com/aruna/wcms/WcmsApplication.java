8c02f17c31f92358664a3b78c15bfc777e0b2c46
Aruna <admgbit@gmail.com>
2026-01-01 23:35:14 +0530

    first commit

---
+++ src/main/java/com/aruna/wcms/WcmsApplication.java
@@ -0,0 +1,13 @@
+package com.aruna.wcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WcmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aruna/wcms/WcmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aruna.wcms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WcmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
