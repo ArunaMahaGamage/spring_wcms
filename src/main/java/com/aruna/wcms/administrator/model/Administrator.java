109b2ed63c7f0bc3e0eca1e5045f1ddeb266bd4b
Aruna <admgbit@gmail.com>
2026-02-09 21:31:50 +0530

    Administrator

--- src/main/java/com/aruna/wcms/administrator/model/Administrator.java
+++ src/main/java/com/aruna/wcms/administrator/model/Administrator.java
@@ -21,5 +21,139 @@ public class Administrator {
 
     private String address;
 
+    private String email;
+
     private String mobileNumber;
+
+    private String positon;
+
+    private String department;
+
+    private String employeeNumber;
+
+    private String provincial;
+
+    private String district;
+
+    private String divisionalSecretaryDivisions;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getIDNumber() {
+        return IDNumber;
+    }
+
+    public void setIDNumber(String IDNumber) {
+        this.IDNumber = IDNumber;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getPositon() {
+        return positon;
+    }
+
+    public void setPositon(String positon) {
+        this.positon = positon;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(String employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
+    public String getProvincial() {
+        return provincial;
+    }
+
+    public void setProvincial(String provincial) {
+        this.provincial = provincial;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getDivisionalSecretaryDivisions() {
+        return divisionalSecretaryDivisions;
+    }
+
+    public void setDivisionalSecretaryDivisions(String divisionalSecretaryDivisions) {
+        this.divisionalSecretaryDivisions = divisionalSecretaryDivisions;
+    }
 }
