802f122a99d34af6b1b9ea70244b38f9e2d7a762
Aruna <admgbit@gmail.com>
2026-03-21 20:10:49 +0530

    Request New Vehicle Route

--- src/main/java/com/aruna/wcms/requestNewVehicleRoute/repository/RequestNewVehicleRouteRepository.java
+++ src/main/java/com/aruna/wcms/requestNewVehicleRoute/repository/RequestNewVehicleRouteRepository.java
@@ -3,5 +3,11 @@ package com.aruna.wcms.requestNewVehicleRoute.repository;
 import com.aruna.wcms.requestNewVehicleRoute.model.RequestNewVehicleRoute;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface RequestNewVehicleRouteRepository extends CrudRepository<RequestNewVehicleRoute, Integer> {
+
+    Optional<RequestNewVehicleRoute> findByIdNumber(String idNumber);
+
+    Optional<RequestNewVehicleRoute> findByEmail(String email);
 }
--- src/main/java/com/aruna/wcms/requestNewVehicleRoute/service/RequestNewVehicleRouteServiceImpl.java
+++ src/main/java/com/aruna/wcms/requestNewVehicleRoute/service/RequestNewVehicleRouteServiceImpl.java
@@ -1,9 +1,12 @@
 package com.aruna.wcms.requestNewVehicleRoute.service;
 
+import com.aruna.wcms.exceptions.ResourceNotFoundException;
 import com.aruna.wcms.requestNewVehicleRoute.model.RequestNewVehicleRoute;
 import com.aruna.wcms.requestNewVehicleRoute.repository.RequestNewVehicleRouteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class RequestNewVehicleRouteServiceImpl implements RequestNewVehicleRouteService {
@@ -14,17 +17,45 @@ public class RequestNewVehicleRouteServiceImpl implements RequestNewVehicleRoute
 
     @Override
     public RequestNewVehicleRoute createRequestNewVehicleRoute(RequestNewVehicleRoute requestNewVehicleRoute) {
-        return null;
+        if (!requestNewVehicleRoute.getIdNumber().isEmpty()) {
+            RequestNewVehicleRoute requestNewVehicleRouteResponse =  requestNewVehicleRouteRepository.save(requestNewVehicleRoute);
+            /*RequestNewVehicleRoute requestNewVehicleRouteSignIn = new RequestNewVehicleRoute();
+            requestNewVehicleRouteSignIn.setIdNumber(requestNewVehicleRoute.getIdNumber());
+            requestNewVehicleRouteSignIn.setUserID(requestNewVehicleRoute.getEmail());
+            requestNewVehicleRouteSignIn.setPassword(requestNewVehicleRoute.getPassword());
+            new CitizenSignUpApiCall().callApiCitizenSignIn(requestNewVehicleRouteSignIn);*/
+            return requestNewVehicleRouteResponse;
+        } else {
+            return null;
+        }
     }
 
     @Override
     public RequestNewVehicleRoute readRequestNewVehicleRoute(RequestNewVehicleRoute requestNewVehicleRoute) {
-        return null;
+        Optional<RequestNewVehicleRoute> requestNewVehicleRouteResult = requestNewVehicleRouteRepository.findById(requestNewVehicleRoute.getId());
+
+        // Handle the Optional result
+        if (requestNewVehicleRouteResult.isPresent()) {
+            return requestNewVehicleRouteResult.get();
+        } else {
+            // Or throw an exception, return null, etc.
+            throw new RuntimeException("Product not found for id :: " + requestNewVehicleRoute.getId());
+        }
     }
 
     @Override
     public RequestNewVehicleRoute updateRequestNewVehicleRoute(RequestNewVehicleRoute requestNewVehicleRoute) {
-        return null;
+        return requestNewVehicleRouteRepository.findById(requestNewVehicleRoute.getId()).map(requestNewVehicleRouteUpdate -> {
+            requestNewVehicleRouteUpdate.setAddress(requestNewVehicleRoute.getAddress());
+            requestNewVehicleRouteUpdate.setDescription(requestNewVehicleRoute.getDescription());
+            // Update other properties as needed
+
+            // The save method is not strictly necessary inside a @Transactional
+            // service method because changes to managed entities are flushed
+            // automatically at transaction commit, but calling it makes the
+            // intention clear and works in non-transactional contexts too.
+            return requestNewVehicleRouteRepository.save(requestNewVehicleRouteUpdate);
+        }).orElseThrow(() -> new ResourceNotFoundException("User not found with id " + requestNewVehicleRoute.getId()));
     }
 
     @Override
