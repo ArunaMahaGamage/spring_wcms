38e7605454c70e6bf3c856ea1968c4fb0ce3715e
Aruna <admgbit@gmail.com>
2026-03-21 14:23:53 +0530

    Request New Vehicle Route

--- src/main/java/com/aruna/wcms/requestNewBin/model/RequestNewBin.java
+++ src/main/java/com/aruna/wcms/requestNewBin/model/RequestNewBin.java
@@ -1,4 +1,80 @@
 package com.aruna.wcms.requestNewBin.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class RequestNewBin {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Integer id;
+
+    private String email;
+
+    private String address;
+
+    private String idNumber;
+
+    private String description;
+
+    private String lat;
+
+    private String lon;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLon() {
+        return lon;
+    }
+
+    public void setLon(String lon) {
+        this.lon = lon;
+    }
 }
