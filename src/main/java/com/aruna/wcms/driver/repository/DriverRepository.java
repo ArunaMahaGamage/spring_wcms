a8b9156d89527e74cd2dd5295bd7c9327ff12738
Aruna <admgbit@gmail.com>
2026-03-31 08:48:20 +0530

    add searching query

--- src/main/java/com/aruna/wcms/driver/repository/DriverRepository.java
+++ src/main/java/com/aruna/wcms/driver/repository/DriverRepository.java
@@ -4,6 +4,14 @@ import com.aruna.wcms.driver.model.Driver;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DriverRepository extends CrudRepository<Driver, Integer> {
+
+    Optional<Driver> findByDriverIdNumber (String driverIdNumber);
+
+    Optional<Driver> findByDriverLicenceNumber (String driverLicenceNumber);
+
+    Optional<Driver> findByEmail (String email);
 }
